575305cfbae6942309a7df4ff3e2d1c73ff303bb
libang <libang@giga.build>
2016-08-24 00:05:54 +0800

    init

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import App from './App';
+import VueRouter from 'vue-router';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App },
-});
+Vue.use(VueRouter);
+
+const router = new VueRouter({ history: true });
+
+router.start(App, '#app');
