c3a1fdcdefb0bb16332bd4212e18a099a822457a
libang <libang@giga.build>
2016-10-16 19:09:51 +0800

    change to deploy

--- web/src/vuex/actions/urls.js
+++ web/src/vuex/actions/urls.js
@@ -1,2 +1,4 @@
-export const ROOT = 'http://localhost:3000/api';
+export const ROOT = process.env.NODE_ENV !== 'production' ? 'http://music.api.socket/api' : 'http://localhost:3000/api';
+
+
 export const SEARCH = 'search';
--- web/src/vuex/plugin/index.js
+++ web/src/vuex/plugin/index.js
@@ -2,5 +2,10 @@ import createLogger from 'vuex/dist/logger';
 import audioProgress from './audioProgress';
 import autoStop from './autoStop';
 
+const plugins = () => {
+  const basePlugins = [audioProgress, autoStop];
+  if (process.env.NODE_ENV !== 'production') return basePlugins.concat([createLogger()]);
+  return basePlugins;
+};
 
-export default [createLogger(), audioProgress, autoStop];
+export default plugins();
